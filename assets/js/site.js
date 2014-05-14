47961197d972d2b3b5029b89a8ef584229dd48e1
wsswdl <308769907@qq.com>
2014-05-14 22:09:30 +0800

    update

--- assets/js/site.js
+++ assets/js/site.js
@@ -24,6 +24,29 @@ var site = {
 				onload: site.Handlers.onGCSEAPILoad
 			});
 		}
+
+		$(document).ready(function(){
+//首先将#back-to-top隐藏
+ $("#back-top").hide();
+//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+$(function () {
+$(window).scroll(function(){
+if ($(window).scrollTop()>100){
+$("#back-top").fadeIn(1500);
+}
+else
+{
+$("#back-top").fadeOut(1500);
+}
+});
+//当点击跳转链接后，回到页面顶部位置
+$("#back-top").click(function(){
+$('body,html').animate({scrollTop:0},1000);
+return false;
+});
+});
+});
+
 	},
 	
 	Handlers: {
@@ -143,24 +166,3 @@ $(document).ready(function () {
 });
 */
 
-$(document).ready(function(){
-//首先将#back-to-top隐藏
- $("#back-top").hide();
-//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
-$(function () {
-$(window).scroll(function(){
-if ($(window).scrollTop()>100){
-$("#back-top").fadeIn(1500);
-}
-else
-{
-$("#back-top").fadeOut(1500);
-}
-});
-//当点击跳转链接后，回到页面顶部位置
-$("#back-top").click(function(){
-$('body,html').animate({scrollTop:0},1000);
-return false;
-});
-});
-});
