4c76b39e813d1953db23baa1f0bde710ea9df67b
wsswdl <308769907@qq.com>
2014-05-14 21:27:19 +0800

    update

--- assets/js/site.js
+++ assets/js/site.js
@@ -125,19 +125,3 @@ elf(function () {
 	});
 });
 
-$("#back-top").hide();
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1000) {
-      $('#back-top').fadeIn();
-    } else {
-      $('#back-top').fadeOut();
-    }
-  });
-  $('#back-top a').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
-    return false;
-  });
-});
