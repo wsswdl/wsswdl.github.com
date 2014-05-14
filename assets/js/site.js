983ff68b95ef2c7702ce85c23216da06f98dfd33
wsswdl <308769907@qq.com>
2014-05-14 22:20:30 +0800

    update

--- assets/js/site.js
+++ assets/js/site.js
@@ -25,28 +25,6 @@ var site = {
 			});
 		}
 
-		$(document).ready(function(){
-//首先将#back-to-top隐藏
- $("#back-top").hide();
-//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
-$(function () {
-$(window).scroll(function(){
-if ($(window).scrollTop()>100){
-$("#back-top").fadeIn(1500);
-}
-else
-{
-$("#back-top").fadeOut(1500);
-}
-});
-//当点击跳转链接后，回到页面顶部位置
-$("#back-top").click(function(){
-$('body,html').animate({scrollTop:0},1000);
-return false;
-});
-});
-});
-
 	},
 	
 	Handlers: {
@@ -166,3 +144,24 @@ $(document).ready(function () {
 });
 */
 
+$(document).ready(function(){
+//首先将#back-to-top隐藏
+ $("#back-top").hide();
+//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+$(function () {
+$(window).scroll(function(){
+if ($(window).scrollTop()>100){
+$("#back-top").fadeIn(1500);
+}
+else
+{
+$("#back-top").fadeOut(1500);
+}
+});
+//当点击跳转链接后，回到页面顶部位置
+$("#back-top").click(function(){
+$('body,html').animate({scrollTop:0},1000);
+return false;
+});
+});
+});
